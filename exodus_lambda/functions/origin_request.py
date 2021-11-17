ecd6146521ccaf5b51224e5f99452628e89b717e
Rohan McGovern <rmcgover@redhat.com>
2021-11-18 09:07:40 +1000

    WIP: test addition of a secret

--- exodus_lambda/functions/origin_request.py
+++ exodus_lambda/functions/origin_request.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import time
 import urllib
@@ -5,8 +6,60 @@ from datetime import datetime, timedelta, timezone
 
 import boto3
 import cachetools
+from botocore.exceptions import ClientError
 
 from .base import LambdaBase
+
+
+def get_secret(arn, logger):
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name="secretsmanager", region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=arn)
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "DecryptionFailureException":
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InvalidParameterException":
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "InvalidRequestException":
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if "SecretString" in get_secret_value_response:
+            secret = get_secret_value_response["SecretString"]
+            logger.warning("secret string %s", repr(secret)[0:50])
+        else:
+            decoded_binary_secret = base64.b64decode(
+                get_secret_value_response["SecretBinary"]
+            )
+            logger.warning(
+                "secret binary %s", repr(decoded_binary_secret)[0:50]
+            )
 
 
 class OriginRequest(LambdaBase):
@@ -20,6 +73,16 @@ class OriginRequest(LambdaBase):
             ).total_seconds(),
             timer=time.monotonic,
         )
+
+    @property
+    def cookie_key(self):
+        out = self._cache.get("secret")
+        if out is None:
+            secret_arn = self.conf.get("secret")
+            out = get_secret(secret_arn, self.logger)
+            self._cache["secret"] = out
+        # TODO: complete me
+        return out
 
     @property
     def definitions(self):
