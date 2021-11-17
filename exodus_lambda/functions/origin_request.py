f6936ea46a82710165ea5fb1834dd21842bbb126
Rohan McGovern <rmcgover@redhat.com>
2021-11-18 09:53:52 +1000

    Don't catch

--- exodus_lambda/functions/origin_request.py
+++ exodus_lambda/functions/origin_request.py
@@ -11,7 +11,7 @@ from botocore.exceptions import ClientError
 from .base import LambdaBase
 
 
-def get_secret(arn, logger):
+def get_secret(arn, logger) -> str:
     region_name = "us-east-1"
 
     logger.warning("attempting to get secret %s", arn)
@@ -26,44 +26,20 @@ def get_secret(arn, logger):
     # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     # We rethrow the exception by default.
 
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=arn)
-    except ClientError as e:
-        if e.response["Error"]["Code"] == "DecryptionFailureException":
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "InternalServiceErrorException":
-            # An error occurred on the server side.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "InvalidParameterException":
-            # You provided an invalid value for a parameter.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "InvalidRequestException":
-            # You provided a parameter value that is not valid for the current state of the resource.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
-        elif e.response["Error"]["Code"] == "ResourceNotFoundException":
-            # We can't find the resource that you asked for.
-            # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
+    get_secret_value_response = client.get_secret_value(SecretId=arn)
+
+    # Decrypts secret using the associated KMS CMK.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if "SecretString" in get_secret_value_response:
+        secret = get_secret_value_response["SecretString"]
+        logger.warning("secret string %s", repr(secret)[0:50])
+        return "string-%s" % secret
     else:
-        # Decrypts secret using the associated KMS CMK.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if "SecretString" in get_secret_value_response:
-            secret = get_secret_value_response["SecretString"]
-            logger.warning("secret string %s", repr(secret)[0:50])
-            return "string-%s" % secret
-        else:
-            decoded_binary_secret = base64.b64decode(
-                get_secret_value_response["SecretBinary"]
-            )
-            logger.warning(
-                "secret binary %s", repr(decoded_binary_secret)[0:50]
-            )
-            return "binary-%s" % decoded_binary_secret
+        decoded_binary_secret = base64.b64decode(
+            get_secret_value_response["SecretBinary"]
+        )
+        logger.warning("secret binary %s", repr(decoded_binary_secret)[0:50])
+        return "binary-%s" % decoded_binary_secret
 
 
 class OriginRequest(LambdaBase):
